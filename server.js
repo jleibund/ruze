86b976d8c6086e650f4462be232855cba8f50772
jleibund <jleibund@gmail.com>
2013-02-18 16:52:45 -0500

    nada

--- server.js
+++ server.js
@@ -10,15 +10,17 @@ app.configure(function(){
 })
 
 var Camel = require('./index.js');
-var camel = new Camel({preload:['header','process']});
+var camel = new Camel({preload:['header','process','format']});
 
 camel.define(function(){
     camel.from('console:in').header().add('what','color').to('direct:a');
     camel.from('direct:a')
         .process(function(exchange,next){
             console.log('process:  header contains-- ',exchange.in.header);
+            exchange.in.body = '{\"statement\":\"'+exchange.in.body+'\"}';
             next();
         })
+        .marshall().json()
         .to('console:out');
 }).then(function(){
     return camel.start();
