bc98b32be8e4f207aefa2bacab9bf7a9302612ed
jleibund <jleibund@gmail.com>
2013-03-05 09:37:57 -0500

    extend issue

--- public/js/main.js
+++ public/js/main.js
@@ -22,7 +22,7 @@ requirejs(['ruze/ruze','jquery','text!conf/ruze.json','socket.io'], function(Ruz
         ruze.from('dom:h1.project?on=click')
             .expr('in.body={timestamp:in.body.timeStamp, text:in.body.currentTarget.outerText, type:in.body.type}')
             .to('myserver:direct:a')
-            .to('console:out');
+            .to('local:console:out');
 
 //        ruze.from('direct:a')
 //            .to('console:out')
