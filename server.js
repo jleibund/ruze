79f8a7f7554e79c9ee9dcd255dd1ce37ebb31c64
jleibund <jleibund@gmail.com>
2013-03-21 00:01:38 -0400

    - refactor complete

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.configure(function(){
 })
 
 var Ruze = require('./index.js');
+//var ruze = new Ruze({preload:['process','expr'],listen:ioServer});
 var ruze = new Ruze({preload:['process','expr'],listen:ioServer, io:io, connect:{server2:'http://localhost:3000/events'}});
 
 ruze.loaders.local.addPath('../extras/server')
